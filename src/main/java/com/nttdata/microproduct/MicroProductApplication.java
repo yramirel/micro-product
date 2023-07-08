5cb180b515c9ad23352d5ed89688b5edfc45b67e
yramirel <yelsinkennedy.ramirezlujan@emeal.nttdata.com>
2023-07-08 11:32:58 -0500

    init

---
+++ src/main/java/com/nttdata/microproduct/MicroProductApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.microproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/microproduct/MicroProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.microproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
